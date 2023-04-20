31585978ee252db519c7c81e425ab7414937a5fc
bNoahrobl <noahrobl70@gmail.com>
2023-04-20 12:25:28 +0200

    new component added

--- f1-webshop/src/app/app.module.ts
+++ f1-webshop/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponentComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
