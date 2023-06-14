d58b20f8bcd6d7658d564b6f1acb83c3a4657925
bNoahrobl <noahrobl70@gmail.com>
2023-06-14 10:25:16 +0200

    About us
    Added about us and styled it

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,69 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h2>About Us</h2>
+    <p>
+      Welcome to our Formula 1 webshop! We are passionate about motorsports and
+      strive to bring you the best selection of Formula 1 merchandise and
+      memorabilia. Whether you're a fan of a specific team or driver, or just
+      love the thrill of high-speed racing, we have something for everyone.
+    </p>
+    <p>
+      Our team of dedicated enthusiasts hand-picks each item in our shop to
+      ensure the highest quality and authenticity. We work directly with
+      manufacturers and official Formula 1 partners to bring you officially
+      licensed products that celebrate the excitement and prestige of Formula 1.
+    </p>
+    <h3>Our Mission</h3>
+    <p>
+      Our mission is to provide Formula 1 fans with a one-stop shop for all
+      their racing needs. We want to create an immersive experience where fans
+      can find everything they're looking for, from team apparel and accessories
+      to collectibles and signed memorabilia.
+    </p>
+    <h3>Contact Us</h3>
+    <p>
+      We value your feedback and are always here to assist you. If you have any
+      questions, suggestions, or concerns, please don't hesitate to reach out to
+      us. You can contact our customer support team via email at
+      <a href="mailto:noah.robl@student.htldornbirn.at">info@formula1webshop.com</a> or
+      give us a call at <a href="callto:+436641016659">+43 664 1016659</a>
+    </p>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'AboutUs',
+};
+</script>
+
+<style scoped>
+.about {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.about h1 {
+  font-size: 28px;
+  margin-bottom: 20px;
+}
+
+.about h2 {
+  font-size: 24px;
+  margin-top: 30px;
+  margin-bottom: 10px;
+}
+
+.about p {
+  font-size: 16px;
+  line-height: 1.5;
+  margin-bottom: 20px;
+}
+
+.about p:last-child {
+  margin-bottom: 0;
+}
+</style>
 
 
